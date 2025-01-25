547c600944401f4370925a04f179574130898481
NicholasH15 <n_henricksen@u.pacific.edu>
2025-01-25 15:59:54 -0800

    Scan function v1

--- scan.py
+++ scan.py
@@ -1,6 +1,22 @@
 import socket
 import argparse
 
+def scan_port(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((ip,port))
+        if result == 0:
+            print(f"Port {port}: Open")
+        else:
+            print(f"Port {port}: Closed")
+            sock.close()
+    except KeyboardInterrupt:
+        print("\nScan interrupted by user.")
+        exit()
+    except Exception as e:
+        print(f"Error scanning port {port}: {e}")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Simple Port Scanner")
     parser.add_argument("ip", help = "Target IP address or hostname")
@@ -13,3 +29,5 @@ if __name__ == "__main__":
     end_port = args.end_port
 
     print(f"Scanning {ip} from {start_port} to {end_port}...")
+    for port in range(start_port, end_port + 1):
+        scan_port(ip, port)
